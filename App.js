37c7e8369229fec19e8d6d0051054f8e4d4ce484
Ignacio Pastor <ignaciopastorcoello@gmail.com>
2018-05-03 00:21:10 +0200

    Add react-native-material-ui

--- App.js
+++ App.js
@@ -1,5 +1,23 @@
 import React from 'react';
-import { FlatList, ActivityIndicator, Text, View  } from 'react-native';
+import { FlatList, ActivityIndicator, Text, View, StyleSheet, StatusBar } from 'react-native';
+import { COLOR, ThemeProvider, Toolbar, Subheader, Card, ListItem } from 'react-native-material-ui';
+
+const uiTheme = {
+    palette: {
+        primaryColor: COLOR.green500,
+        accentColor: COLOR.pink500,
+    },
+};
+
+const styles = StyleSheet.create({
+    container: {
+        paddingTop: 20,
+    },
+    textContainer: {
+        paddingHorizontal: 16,
+        paddingBottom: 16,
+    },
+});
 
 export default class Asignaturas extends React.Component {
 
@@ -39,19 +57,40 @@ export default class Asignaturas extends React.Component {
 
     if(this.state.isLoading){
       return(
-        <View style={{flex: 1, padding: 20}}>
+        <View>
+           <StatusBar
+                barStyle = "light-content" 
+           />
           <ActivityIndicator/>
         </View>
       )
     }
 
     return(
-      <View style={{flex: 1, paddingTop:20}}>
-        <FlatList
-          data={this.state.dataSource}
-          renderItem={({item}) => <Text>{item.IdAsignatura}</Text>}
-          keyExtractor={(item, index) => 'index'}
-        />
+      <View style={{flex: 1}}>
+        <ThemeProvider uiTheme={uiTheme}>
+          <View style={{flex: 1}}>
+            <View style={styles.container}>
+              <Toolbar centerElement="Asignaturas" />
+            </View>
+            <View style={{flex: 1}}>
+              <FlatList
+                data={this.state.dataSource}
+                renderItem={({item}) => 
+                  <Card>
+                      <ListItem
+                          centerElement={{
+                              primaryText: item.IdAsignatura,
+                              secondaryText: 'Descripción',
+                          }}
+                      />
+                  </Card>
+                }
+                keyExtractor={(item, index) => 'index'}
+              />
+            </View>
+          </View>
+        </ThemeProvider>
       </View>
     );
   }
